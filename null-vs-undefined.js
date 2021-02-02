03c5e7ebc630438da9c3ccbff3498611f65f2995
Ratul <ratulkhan1428@gmail.com>
2021-02-02 20:06:17 +0600

    null vs undefined cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+let Babu;
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const person = {name: "John", phone: 789339};
+console.log(person.address);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [12, 14, 19];
+console.log(ages[9]);
